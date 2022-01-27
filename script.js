755d46bf3965554639eceabb1ec1c764f9c2f935
anshulg954 <admin@admins-MacBook-Air.local>
2022-01-28 00:25:09 +0530

    Testing using Mocha #trainingWeek

--- script.js
+++ script.js
@@ -2,21 +2,21 @@
 // * exactly same keys and values
 export function checkEqualObjects(object1, object2) {
   // Get all keys of objects as an iterable object
-    const keys1 = Object.keys(object1);
-    const keys2 = Object.keys(object2);
+    const keysObject1 = Object.keys(object1);
+    const keysObject2 = Object.keys(object2);
     // Check if the length of the keys are equal
-    if (keys1.length !== keys2.length) {
+    if (keysObject1.length !== keysObject2.length) {
       return false;
     }
-    // Check if the keys are equal
-    for (const key of keys1) {
+    // Check if the keys are equal for each key in the object
+    for (const key of keysObject1) {
       // Get the value of the keys from both objects
-      const val1 = object1[key];
-      const val2 = object2[key];
+      const value_1 = object1[key];
+      const value_2 = object2[key];
       // Check if the values are objects if so recursively call the function
-      const ifObjects = checkForObject(val1) && checkForObject(val2);
-      // if both are objects, recursively call the function else if the values are not equal return false
-      if (ifObjects && !checkEqualObjects(val1, val2) || !ifObjects && val1 !== val2){
+      const ifObjects = checkForObject(value_1) && checkForObject(value_2);
+      //if the values are not equal return false or if the values are objects, recursively call the function 
+      if (!ifObjects && value_1 !== value_2 || ifObjects && !checkEqualObjects(value_1, value_2)) {
         return false;
       }
     }
@@ -25,5 +25,5 @@ export function checkEqualObjects(object1, object2) {
   }
   // * check if the value is an object
   function checkForObject(object) {
-    return object != null && typeof object === 'object';
+    return typeof object === 'object' && object != null;
   }
